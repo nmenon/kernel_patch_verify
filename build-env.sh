be388470f6b36573ed4061be45ac8821b488a42c
Andrew Davis <afd@ti.com>
2022-04-11 11:02:01 -0500

    build-env.sh: Do not print out every file that we extract
    This is way too verbose when extracting all of GCC, and fills our
    screen and output buffer when doing docker builds. Reduce verbosity.
    
    Signed-off-by: Andrew Davis <afd@ti.com>

--- build-env.sh
+++ build-env.sh
@@ -102,14 +102,14 @@ download_and_install_armgcc()
 	F64='aarch64-gcc.tar.xz'
 	URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu.tar.xz?revision=61c3be5d-5175-4db6-9030-b565aae9f766&la=en&hash=0A37024B42028A9616F56A51C2D20755C5EBBCD7"
 	aria2c $ARIA_OPTS -o "$F64" "$URL"
-	tar -C /usr/local/cross-gcc-linux-9/ --strip-components=1 -xvf "$F64"
+	tar -C /usr/local/cross-gcc-linux-9/ --strip-components=1 -xf "$F64"
 	rm -f "$F64"
 
 	#arch32
 	F32='aarch32-gcc.tar.xz'
 	URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf.tar.xz?revision=fed31ee5-2ed7-40c8-9e0e-474299a3c4ac&la=en&hash=76DAF56606E7CB66CC5B5B33D8FB90D9F24C9D20"
 	aria2c $ARIA_OPTS -o "$F32" "$URL"
-	tar -C /usr/local/cross-gcc-linux-9/ --strip-components=1 -xvf "$F32"
+	tar -C /usr/local/cross-gcc-linux-9/ --strip-components=1 -xf "$F32"
 	rm -f "$F32"
 }
 
