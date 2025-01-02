e3b1b04fb3925774aa771665e7502c91d18856db
Nishanth Menon <nm@ti.com>
2025-01-02 07:32:34 -0600

    build-env.sh: Bump to v2024.11 dt-schema
    Update dt-schema to latest v2024.11 schema and while at it, update other
    packages to the latest revs as well.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- build-env.sh
+++ build-env.sh
@@ -6,17 +6,17 @@ shopt -s expand_aliases
 NPROC=$(nproc)
 
 # https://git.kernel.org/pub/scm/git/git.git/
-export GIT_TAG=2.46.2
+export GIT_TAG=2.47.1
 # https://git.kernel.org/pub/scm/utils/dtc/dtc.git
-export DTC_TAG=v1.7.1
+export DTC_TAG=v1.7.2
 # https://git.kernel.org/pub/scm/devel/sparse/sparse.git
 export SPARSE_TAG=master
 # https://repo.or.cz/smatch.git
 export SMATCH_TAG=master
 # https://github.com/coccinelle/coccinelle/tags
-export COCCI_TAG=1.2
+export COCCI_TAG=1.3.0
 # https://github.com/devicetree-org/dt-schema/tags
-export DTSCHEMA_REV=v2024.09
+export DTSCHEMA_REV=v2024.11
 
 ARIA_OPTS=( --summary-interval=5 --timeout=180 --retry-wait=10 -m 0 -x 10 -j 10 )
 
