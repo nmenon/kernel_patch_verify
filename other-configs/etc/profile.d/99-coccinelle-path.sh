d4ad3c7f0c6c380a2a56ba13e2312c6fe1afdca2
Nishanth Menon <nm@ti.com>
2026-01-14 13:56:38 -0600

    other-configs: coccinelle point it to native install
    In commit 13e103ebbdc0 ("build-env.sh: coccinelle: Switch to prebuilt
    package") we switched to native install of coccinelle. however, in the
    container environment, paths still point to the path that would have
    been used if locally built.. uggh.. Fix the same.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- other-configs/etc/profile.d/99-coccinelle-path.sh
+++ other-configs/etc/profile.d/99-coccinelle-path.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/coccinelle/python
-export COCCINELLE_HOME=/usr/local/lib/coccinelle/
+export PYTHONPATH=$PYTHONPATH:/usr/lib/coccinelle/python
+export COCCINELLE_HOME=/usr/lib/coccinelle/
